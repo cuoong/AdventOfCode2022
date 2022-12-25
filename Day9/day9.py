6ffeb2b1d04df2a962802eb0df571667cb1117b9
cuong <cuongnt04230@gmail.com>
2022-12-25 21:44:09 +0700

    day 9 part 1

--- Day9/day9.py
+++ Day9/day9.py
@@ -1,6 +1,28 @@
 import sys
+import numpy as np
 
 file = sys.argv[1] if len(sys.argv) > 1 else '9.in'
 data = open(file).readlines()
+head = (0, 0)
+tail = (0, 0)
+visited = set()
+visited.add(tail)
 
-line = [x.strip() for x in data]
+for ele in data:
+    dir, steps = ele.split()
+    for _ in range(int(steps)):
+        match dir:
+            case 'U':
+                head = (head[0], head[1] + 1)
+            case 'D':
+                head = (head[0], head[1] - 1)
+            case 'L':
+                head = (head[0] - 1, head[1])
+            case 'R':
+                head = (head[0] + 1, head[1])
+        diff_x = head[0] - tail[0]
+        diff_y = head[1] - tail[1]
+        if abs(diff_x) > 1 or abs(diff_y) > 1:
+            tail = (tail[0] + np.sign(diff_x), tail[1] + np.sign(diff_y))
+            visited.add(tail)
+print(len(visited))
